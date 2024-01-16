917b0e737d903c18af561398c274b3dd31cff1aa
yaraAawad <awadyara2000@gmail.com>
2024-01-16 19:48:09 +0300

    commit
    initial

--- code.js
+++ code.js
@@ -9,7 +9,32 @@ var urlEncodded=body.urlencoded({extended:false})
 
 app.use(express.static('public'))
 
+var product4={"product 4":{
+    "name":"Glass Straw",
+    "collection":"Straws",
+    "room":"kitchen",
+    "section":"Home",
+    "product_id":"4"
+}}
 
+// Retrieve the main page -> Get 
+app.get('/',function(req,res)
+{
+res.sendFile(__dirname+'/public/main.html')
+})
+
+// Retrieve the form page -> Get 
+app.get('/form',function(req,res)
+{
+res.sendFile(__dirname+'/public/form.html')
+})
+
+// Get All products -> Get
+app.get('/products',function(req,res)
+{
+    res.json(products)
+
+})
 
 var server = app.listen(9000, function () {
     var host = server.address().address
@@ -17,4 +42,4 @@ var server = app.listen(9000, function () {
     console.log("Start")
  })
 
- 
+
